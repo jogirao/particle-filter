632eb85996a010934c6a83f0037c296d4ecda992
jogirao <joao.mllp.girao@hotmail.com>
2020-06-16 18:11:51 +0100

    Added region

---
+++ region.py
@@ -0,0 +1,24 @@
+class Region():
+
+    import heapq as hq
+    
+    def __init__(self, points=[(0,0),(0,1),(1,1),(1,0)]):
+        self.corners=points
+        
+    def intersect(self, x_i, y_i, x_f, y_f):
+        # Computes the intersection points of the input line with the region borders, sorted ascendingly
+        h=[]
+        for i in range(len(self.corners)):
+            denominator=(x_i-x_f)*(self.corners[i-1][1]-self.corners[i][1])-(y_i-y_f)*(self.corners[i-1][0]-self.corners[i][0])
+            # Non-parallel or coincident lines
+            if denominator!=0:
+                t=((x_i-self.corners[i-1][0])*(self.corners[i-1][1]-self.corners[i][1])-(y_i-self.corners[i-1][1])*(self.corners[i-1][0]-self.corners[i][0]))/denominator
+                u=((x_i-self.corners[i-1][0])*(y_i-y_f)-(x_i-x_f)*(y_i-self.corners[i-1][1]))/denominator
+                if not (t>1 or t<0 or u>1 or u<0):
+                    # Lines intersect at point P
+                    P=(x_i + t*(x_f-x_i), y_i + t*(y_f-y_i))
+                    priority_point=(abs(P[0]-x_i)+abs(P[1]-y_i),P)
+                    if priority_point not in h:
+                        self.hq.heappush(h,priority_point)
+        return h
+                    
