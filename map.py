7d595fe542997fc9c98371f5f9ace99b6756b90b
jogirao <joao.mllp.girao@hotmail.com>
2024-03-18 22:42:20 +0000

    Refactor map.py
    Refactoring code in map.py

--- map.py
+++ map.py
@@ -1,45 +1,83 @@
-from region import Region
-import math
-import heapq as hq
+from json import loads
+import matplotlib.pyplot as plt
+from math import floor, ceil
 
 
 class Map:
-    x_min, x_max, y_min, y_max = 0, 1, 0, 1
 
-    def __init__(self, boundaries=None, obstacles=None):
-        if boundaries is None:
-            boundaries = [(self.x_min, self.y_min), (self.x_min, self.y_max),
-                          (self.x_max, self.y_max), (self.x_max, self.y_min)]
-        if obstacles is None:
-            obstacles = []
-        self.map_region = Region(boundaries)
-        self.obstacles = [Region(obstacle) for obstacle in obstacles]
+    def __init__(self, map_choice="preset"):
+        self.wall_points_x, self.wall_points_y = [], []
+        self.map_min_x, self.map_max_x, self.map_min_y, self.map_max_y = 0, 0, 0, 0
+        self.build_map(map_choice)
 
-    def get_destination(self, x_i, y_i, x_f, y_f):
-        final_pos = (float("inf"), (x_f, y_f))
-        for region in self.obstacles + [self.map_region]:
-            collisions = region.intersect(x_i, y_i, x_f, y_f)
-            if collisions:
-                first_collision = hq.heappop(collisions)
-                final_pos = min(final_pos, first_collision)
-        return final_pos[1]
+    def build_map(self, map_choice):
+        # Create map from collection of points stored in file
+        # Get points from file
+        with open("MapPointDict.txt", "r") as file:
+            map_points = []
+            while not map_points:
+                string = file.readline()
+                if string[:string.find(":")] == map_choice:
+                    map_points = loads(string[string.find(":")+2:])
 
-    def contains(self, x, y):
-        n_intersections = sum(len(region.intersect(x, y, self.x_min, y)) for region in self.obstacles + [self.map_region])
-        return n_intersections % 2
+        # Generate point series in x,x axis from group of points
+        for wall in map_points:
+            wall_x, wall_y = [], []
+            for point in wall:
+                wall_x.append(point[0]), wall_y.append(point[1])
+            self.wall_points_x.append(wall_x), self.wall_points_y.append(wall_y)
 
-    def edge_intersect_by_pose(self, x, y, angle):
-        quarter_pi = math.pi / 4
-        if quarter_pi < angle <= 3 * quarter_pi:
-            return round((self.y_max - y) / math.tan(angle) + x, 8), self.y_max
-        elif -3 * quarter_pi < angle <= - quarter_pi:
-            return round((self.y_min - y) / math.tan(angle) + x, 8), self.y_min
-        elif -quarter_pi < angle <= quarter_pi:
-            return self.x_max, round((self.x_max - x) * math.tan(angle) + y, 8)
-        else:
-            return self.x_min, round((self.x_min - x) * math.tan(angle) + y, 8)
+        min_x, max_x = min(min(x) for x in self.wall_points_x), max(max(x) for x in self.wall_points_x)
+        min_y, max_y = min(min(y) for y in self.wall_points_y), max(max(y) for y in self.wall_points_y)
+        self.map_min_x, self.map_max_x = min_x - floor(0.1*(max_x-min_x)), max_x + ceil(0.1 * (max_x - min_x))
+        self.map_min_y, self.map_max_y = min_y - floor(0.1*(max_y-min_y)), max_y + ceil(0.1 * (max_y - min_y))
 
-    def plot_map(self, plot):
-        self.map_region.plot_region(plot)
-        for obstacle in self.obstacles:
-            obstacle.plot_region(plot, facecolor='white')
+
+    def intersects(self, p1, q1, p2, q2) -> bool:
+        # Checks if 2 line segments ((p1,q1) and (p2,q2)) intersect
+        o1, o2, o3, o4 = self.orientation(p1, q1, p2), self.orientation(p1, q1, q2), self.orientation(p2, q2, p1), \
+                         self.orientation(p2, q2, q1)
+        if o1 + o2 + o3 + o4 == 0:
+            # Collinear line segments. Check if x-projections intersect
+            max_x, min_x = max(p1[0], p2[0], q1[0], q2[0]), min(p1[0], p2[0], q1[0], q2[0])
+            if max_x - min_x <= abs(q1[0] - p1[0]) + abs(q2[0] - p2[0]):
+                return True
+        elif (o1 != o2) & (o3 != o4):
+            return True
+        return False
+
+    def is_in_field(self, point: list) -> bool:
+        # Counts vertical/horizontal intersections with the walls
+        horiz_hits, vert_hits = 0, 0
+        for nb in range(len(self.wall_points_x)):
+            # For each polygon
+            for edge in range(len(self.wall_points_x[nb])-1):
+                # For each wall, check if horizontal/vertical lines intersect
+                x1, x2, y1, y2 = self.wall_points_x[edge], self.wall_points_x[edge+1], self.wall_points_y[edge], \
+                                 self.wall_points_y[edge+1]
+                if x1 < point[0] < x2:
+                    vert_hits += 1
+                if y1 < point[1] < y2:
+                    horiz_hits += 1
+        if horiz_hits % 2 & vert_hits % 2:
+            return True
+        return False
+
+    @staticmethod
+    def orientation(p1, p2, p3) -> int:
+        # Computes orientation given 3 points
+        val = (p2[1] - p1[1]) * (p3[0] - p2[0]) - (p2[0] - p1[0]) * (p3[1] - p2[1])
+        if val > 0:
+            # Clockwise orientation
+            return 1
+        elif val < 0:
+            # Counterclockwise orientation
+            return 2
+        # Collinear points
+        return 0
+
+    def plot(self, ax):
+        # Plot map
+        for nb in range(len(self.wall_points_x)):
+            ax.plot(self.wall_points_x[nb], self.wall_points_y[nb], linewidth=2.0, color='k')
+            ax.set(xlim=(self.map_min_x, self.map_max_x), ylim=(self.map_min_y, self.map_max_y))
